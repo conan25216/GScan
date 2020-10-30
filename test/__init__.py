30ca8a3188821f3361a2552610bcb24909e5be5a
conan <conan25216@gmail.com>
2020-10-30 10:33:37 +0800

    中文报错测试

---
+++ test/__init__.py
@@ -0,0 +1,5 @@
+#!/bin/python
+# -*- coding:utf-8 -*-
+# Author: conanhu
+# Date: 2020/10/30
+# Desc:
---
+++ test/testEncodeError.py
@@ -0,0 +1,8 @@
+#!/bin/python
+# -*- coding:utf-8 -*-
+# Author: conanhu
+# Date: 2020/10/30
+# Desc:
+
+with open("testEncode.log","a+") as f:
+    f.write(u"我们测试一下为什么写中文报错\n")
